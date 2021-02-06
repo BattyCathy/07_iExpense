1c42e833876e16f0c2c9c866a5612ab7fe0e4c96
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-02-06 14:50:14 -0800

    "Showing and hiding views" notes file added

---
+++ 07_iExpense/04. Showing and Hiding Views.swift
@@ -0,0 +1,10 @@
+//
+//  04 Showing and Hiding Views.swift
+//  07_iExpense
+//
+//  Created by Austin Roach on 2/6/21.
+//
+
+import Foundation
+
+//There are several ways of showing views in SwiftUI, and one of the most basic is a sheet: a new view presented on top of our existing one. On iOS this au
