326d0cbd429059185d2c54ceb7b5be383cf7238e
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-02-06 13:14:32 -0800

    Initial Commit

---
+++ 07_iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  07_iExpense
+//
+//  Created by Austin Roach on 2/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 07_iExpense/_7_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  _7_iExpenseApp.swift
+//  07_iExpense
+//
+//  Created by Austin Roach on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct _7_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
