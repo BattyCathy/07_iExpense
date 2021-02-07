dad1b6df9db297d73034b4fb5da4b4ee552f2ea6
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-02-07 13:29:04 -0800

    Codable implemented

--- 07_iExpense/ContentView.swift
+++ 07_iExpense/ContentView.swift
@@ -6,36 +6,23 @@
 //
 
 import SwiftUI
-struct ContentView: View {
-    @State private var numbers = [Int]()
-    @State private var currentNumber = 1
-    
-    func removeRows(at offsets: IndexSet) {
-        numbers.remove(atOffsets: offsets)
-    }
-    
-    var body: some View {
-        NavigationView {
-            VStack {
-                List {
-                    ForEach(numbers, id:\.self) {
-                        Text("\($0)")
-                    }
-                    .onDelete(perform: removeRows)
-                }
-                
-                Button("Add Number") {
-                    self.numbers.append(self.currentNumber)
-                    self.currentNumber += 1
-                }
-            }
-        }
-        .navigationBarItems(leading: EditButton())
-    }
+
+struct User: Codable {
+    var firstName: String
+    var lastName: String
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    
+    @State private var user = User(firstName: "Taylor", lastName: "Swift")
+    
+    var body: some View {
+        Button("Save User") {
+            let encoder = JSONEncoder()
+            
+            if let data = try? encoder.encode(self.user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
+        }
     }
 }
