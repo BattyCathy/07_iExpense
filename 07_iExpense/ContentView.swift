5e60205e9a263a2b872c7ba19d52aa5d29d5155a
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-02-09 15:33:16 -0800

    Implemented Expense list

--- 07_iExpense/ContentView.swift
+++ 07_iExpense/ContentView.swift
@@ -14,8 +14,28 @@ struct User: Codable {
 
 struct ContentView: View {
     
+    @ObservedObject var expenses = Expenses()
     
     var body: some View {
-        Text("sup")
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                        let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                                        self.expenses.items.append(expense)
+                                    }) {
+                                        Image(systemName: "plus")
+                                    }
+            )
+        }
+    }
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
