3fa9fe34ed69676561c3963208cf37b1b1a476b1
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-02-10 11:58:48 -0800

    Final polish implemented

--- 07_iExpense/AddView.swift
+++ 07_iExpense/AddView.swift
@@ -12,6 +12,7 @@ struct AddView: View {
     @State private var type = "Personal"
     @State private var amount = ""
     @ObservedObject var expenses: Expenses
+    @Environment(\.presentationMode) var presentationMode
     
     static let types = ["Business", "Personal"]
     
@@ -32,6 +33,7 @@ struct AddView: View {
                 if let actualAmount = Int(self.amount) {
                     let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
                     self.expenses.items.append(item)
+                    self.presentationMode.wrappedValue.dismiss()
                 }
             })
         }
